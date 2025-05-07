ef623b93ab86c2095b4414fe49ef8229391d3d3a
Natan Macedo <natan.mac.n@gmail.com>
2025-05-07 15:23:27 -0300

    :sparkles: Adicionado CRUD básico + classe Controller do projeto

--- src/projeto_final_bloco_01/Menu.java
+++ src/projeto_final_bloco_01/Menu.java
@@ -1,8 +1,11 @@
 package projeto_final_bloco_01;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.Scanner;
 
+import projeto_final_bloco_01.controller.FoneController;
+import projeto_final_bloco_01.model.Fone;
 import projeto_final_bloco_01.model.FoneComFio;
 import projeto_final_bloco_01.model.FoneSemFio;
 
@@ -12,13 +15,18 @@ public class Menu {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		int option;
+		FoneController fone = new FoneController();
 		
-		FoneComFio fcf = new FoneComFio("Samsumg PRO X", "Preto", 150, 1, 1, "Intra auricular", true);
-		fcf.visualizar();
+		int option, tipo, cargasBateria, microfone, id;
+		String modelo, cor, formato;
+		float preco;
+		boolean microfoneBoolean = false;
 		
-		FoneSemFio fsf = new FoneSemFio("Phillips", "Branco", 200, 2, 2, "Intra auricular", 4);
-		fsf.visualizar();
+		FoneComFio fcf = new FoneComFio("Samsumg PRO X", "Preto", 150, fone.gerarId(), 1, "Supra-auricular", true);
+		fone.cadastrar(fcf);
+		
+		FoneSemFio fsf = new FoneSemFio("Phillips", "Branco", 200, fone.gerarId(), 3, "Intra-auricular", 4);
+		fone.cadastrar(fsf);
 		
 		while(true) {
 			
@@ -46,30 +54,118 @@ public class Menu {
 				case 1:
 					System.out.println("Cadastrar novo produto");
 					
+					System.out.println("Digite o modelo do produto: ");
+					sc.skip("\\R");
+					modelo = sc.nextLine();
+					
+					System.out.println("Digite o preço do produto: ");
+					preco = sc.nextFloat();
+					
+					System.out.println("Digite a cor do produto: ");
+					sc.skip("\\R");
+					cor = sc.nextLine();
+					
+					System.out.println("Digite o tipo do produto: (1 - USB-C; 2 - P10; 3 - Wireless");
+					tipo = sc.nextInt();
+					
+					System.out.println("Digite o formato do produto: ");
+					sc.skip("\\R");
+					formato = sc.nextLine();
+					
+					switch(tipo) {
+						case 1, 2 -> {
+							System.out.println("Contém microfone? (1 - SIM; 2 - NÃO");
+							microfone = sc.nextInt();
+							if(microfone == 1) {
+								microfoneBoolean = true;
+							} else if(microfone == 2) {
+								microfoneBoolean = false;
+							}
+							fone.atualizar(new FoneComFio(modelo, cor, preco, fone.gerarId(), tipo, formato, microfoneBoolean));
+						}
+						case 3 -> {
+							System.out.println("Quantas cargas de bateria o fone tem?");
+							cargasBateria = sc.nextInt();
+							fone.atualizar(new FoneSemFio(modelo, cor, preco, fone.gerarId(), tipo, formato, cargasBateria));
+						}
+					}
 					
 					keyPress();
 					break;
 				case 2:
 					System.out.println("Listar todos os fones");
 					
+					fone.listarTodos();
 					
 					keyPress();
 					break;
 				case 3:
 					System.out.println("Consultar produto por ID");
 					
+					System.out.println("Digite o ID do produto: ");
+					id = sc.nextInt();
+					
+					fone.procurarPorID(id);
 					
 					keyPress();
 					break;
 				case 4:
 					System.out.println("Atualizar produto");
 					
+					System.out.println("Digite o ID do produto: ");
+					id = sc.nextInt();
+					
+					Optional<Fone> fones = fone.buscarNaCollection(id);
+					
+					if(fones.isPresent()) {
+						System.out.println("Digite o modelo do produto: ");
+						sc.skip("\\R");
+						modelo = sc.nextLine();
+						
+						System.out.println("Digite o preço do produto: ");
+						preco = sc.nextFloat();
+						
+						System.out.println("Digite a cor do produto: ");
+						sc.skip("\\R");
+						cor = sc.nextLine();
+						
+						System.out.println("Digite o tipo do produto: (1 - USB-C; 2 - P10; 3 - Wireless");
+						tipo = sc.nextInt();
+						
+						System.out.println("Digite o formato do produto: ");
+						sc.skip("\\R");
+						formato = sc.nextLine();
+						
+						switch(tipo) {
+							case 1, 2 -> {
+								System.out.println("Contém microfone? (1 - SIM; 2 - NÃO");
+								microfone = sc.nextInt();
+								if(microfone == 1) {
+									microfoneBoolean = true;
+								} else if(microfone == 2) {
+									microfoneBoolean = false;
+								}
+								fone.cadastrar(new FoneComFio(modelo, cor, preco, id, tipo, formato, microfoneBoolean));
+							}
+							case 3 -> {
+								System.out.println("Quantas cargas de bateria o fone tem?");
+								cargasBateria = sc.nextInt();
+								fone.cadastrar(new FoneSemFio(modelo, cor, preco, id, tipo, formato, cargasBateria));
+							}
+						}
+					} else {
+						System.out.printf("\nO produto ID: %d não existe!", id);
+					}
 					
 					keyPress();
 					break;
 				case 5:
 					System.out.println("Deletar produto");
 					
+					System.out.println("Digite o ID do Produto: ");
+					id = sc.nextInt();
+
+					fone.deletar(id);
 					
 					keyPress();
 					break;
